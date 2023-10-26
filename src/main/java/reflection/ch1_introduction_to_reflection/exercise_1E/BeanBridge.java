11d8aed94df66b4048dfa73c75293eefb4af6d49
Dr Heinz M. Kabutz <heinz@javaspecialists.eu>
2023-10-26 20:58:11 +0300

    exercise 1E done

--- src/main/java/reflection/ch1_introduction_to_reflection/exercise_1E/BeanBridge.java
+++ src/main/java/reflection/ch1_introduction_to_reflection/exercise_1E/BeanBridge.java
@@ -1,6 +1,13 @@
 package reflection.ch1_introduction_to_reflection.exercise_1E;
 
+import java.beans.BeanInfo;
 import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.RecordComponent;
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 public class BeanBridge {
     /**
@@ -15,7 +22,29 @@ public class BeanBridge {
     public static <T, R extends Record>
     R convertBeanToRecord(T bean, Class<R> recordType)
             throws ReflectiveOperationException, IntrospectionException {
-        throw new UnsupportedOperationException("TODO");
+        RecordComponent[] recordComponents = recordType.getRecordComponents();
+        Class<?>[] recordComponentTypes = Stream.of(recordComponents)
+                .map(RecordComponent::getType)
+                .toArray(Class<?>[]::new);
+        Object[] recordComponentValues = new Object[recordComponents.length];
+        BeanInfo beanInfo = Introspector.getBeanInfo(bean.getClass());
+        nextRecordComponent:
+        for (int i = 0; i < recordComponents.length; i++) {
+            RecordComponent recordComponent = recordComponents[i];
+            String propertyName = recordComponent.getName();
+            for (PropertyDescriptor descriptor : beanInfo.getPropertyDescriptors()) {
+                if (propertyName.equals(descriptor.getName())) {
+                    recordComponentValues[i] = descriptor.getReadMethod().invoke(bean);
+                    continue nextRecordComponent;
+                }
+            }
+            throw new IllegalArgumentException("Could not find property: " + propertyName);
+        }
+        System.out.println(Arrays.toString(recordComponentTypes));
+        System.out.println(Arrays.toString(recordComponentValues));
+        Constructor<R> constructor = recordType.getConstructor(recordComponentTypes);
+        R record = constructor.newInstance(recordComponentValues);
+        return record;
     }
 
     /**
@@ -30,6 +59,21 @@ public class BeanBridge {
     public static <T extends Record, R>
     R convertRecordToBean(T record, Class<R> beanType)
             throws ReflectiveOperationException, IntrospectionException {
-        throw new UnsupportedOperationException("TODO");
+        Constructor<R> constructor = beanType.getConstructor();
+        R bean = constructor.newInstance();
+        BeanInfo beanInfo = Introspector.getBeanInfo(beanType);
+        nextRecordComponent:
+        for (RecordComponent recordComponent : record.getClass().getRecordComponents()) {
+            String name = recordComponent.getName();
+            for (PropertyDescriptor propertyDescriptor : beanInfo.getPropertyDescriptors()) {
+                if (name.equals(propertyDescriptor.getName())) {
+                    Object value = recordComponent.getAccessor().invoke(record);
+                    propertyDescriptor.getWriteMethod().invoke(bean, value);
+                    continue nextRecordComponent;
+                }
+            }
+            throw new IllegalArgumentException("Could not find property: " + name);
+        }
+        return bean;
     }
 }
