6c3b2a1b2c95e7a681fae6a16fb0bdd30a389d3a
Dr Heinz M. Kabutz <heinz@javaspecialists.eu>
2023-10-26 19:05:27 +0300

    exercise 1A done

--- src/main/java/reflection/ch1_introduction_to_reflection/exercise_1A/ClassExplorer.java
+++ src/main/java/reflection/ch1_introduction_to_reflection/exercise_1A/ClassExplorer.java
@@ -1,5 +1,7 @@
 package reflection.ch1_introduction_to_reflection.exercise_1A;
 
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
 
 public class ClassExplorer {
@@ -7,6 +9,13 @@ public class ClassExplorer {
      * Returns all interfaces in the inheritance hierarchy as a distinct set.
      */
     public static Set<Class<?>> findAllInterfaces(Class<?> clazz) {
-        throw new UnsupportedOperationException("TODO");
+        if (clazz == null) return Set.of();
+        Set<Class<?>> interfaces = new HashSet<>();
+        if (clazz.isInterface()) interfaces.add(clazz);
+        for (Class<?> intf : clazz.getInterfaces()) {
+            interfaces.addAll(findAllInterfaces(intf));
+        }
+        interfaces.addAll(findAllInterfaces(clazz.getSuperclass()));
+        return Set.copyOf(interfaces);
     }
 }
