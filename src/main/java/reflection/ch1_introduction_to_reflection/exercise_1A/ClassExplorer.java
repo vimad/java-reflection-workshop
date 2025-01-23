327f0f15a60a73812715080d39a7f242259dde03
vinod <vinod.madubashana@madmobile.com>
2025-01-23 21:32:06 +0530

    findAllInterfaces solution

--- src/main/java/reflection/ch1_introduction_to_reflection/exercise_1A/ClassExplorer.java
+++ src/main/java/reflection/ch1_introduction_to_reflection/exercise_1A/ClassExplorer.java
@@ -1,5 +1,7 @@
 package reflection.ch1_introduction_to_reflection.exercise_1A;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 
 public class ClassExplorer {
@@ -7,6 +9,21 @@ public class ClassExplorer {
      * Returns all interfaces in the inheritance hierarchy as a distinct set.
      */
     public static Set<Class<?>> findAllInterfaces(Class<?> clazz) {
-        throw new UnsupportedOperationException("TODO");
+        Set<Class<?>> interfaces = new HashSet<>();
+        while (clazz != null) {
+            for (Class<?> iface : clazz.getInterfaces()) {
+                findAllInterfacesRecursively(iface, interfaces);
+            }
+            clazz = clazz.getSuperclass();
+        }
+        return interfaces;
+    }
+
+    private static void findAllInterfacesRecursively(Class<?> iface, Set<Class<?>> interfaces) {
+        if (interfaces.add(iface)) {
+            for (Class<?> superIface : iface.getInterfaces()) {
+                findAllInterfacesRecursively(superIface, interfaces);
+            }
+        }
     }
 }
